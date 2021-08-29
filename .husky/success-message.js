3d6e616ca631b069eb162f4580df87f5c58d7946
Patryk Gurgul <patrykgurgul.it@gmail.com>
2021-08-29 13:18:43 +0200

    style: make my awesome custom success message stand out more

--- .husky/success-message.js
+++ .husky/success-message.js
@@ -1,2 +1,11 @@
 require('console-success');
-console.success('This code is only half bad but I am in a good mood so you can push as an exception');
+console.success(`
+
+
+
+This code is only half bad but I am in a good mood so you can push as an exception
+
+
+
+
+`);
